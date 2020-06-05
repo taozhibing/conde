a5f61aaa273e492f5da05790d60a9fc3d55d3d63
taozhibing <274698983@qq.com>
2020-06-05 13:48:59 +0800

    element引用

---
+++ .history/src/views/Home_20200605134339.vue
@@ -0,0 +1,37 @@
+<template>
+ <div>
+<el-button type="primary">按钮</el-button>
+ </div>
+</template>
+
+<script>
+ export default {
+   name: '',
+   props: {
+   },
+   components: {
+
+   },
+   data () {
+     return {
+
+     }
+   },
+   methods: {
+
+   },
+   mounted() {
+
+   },
+   watch: {
+
+   },
+   computed: {
+
+   }
+ }
+</script>
+
+<style scoped lang='scss'>
+
+</style>
